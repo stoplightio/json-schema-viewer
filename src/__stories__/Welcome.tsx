790a6ed836b3a231180e95e79dae4a539e9a41b5
Jakub Rożek <jakub@stoplight.io>
2019-01-09 12:05:27 +0100

    chore: remove irrelevant code

--- src/__stories__/Welcome.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import { linkTo } from '@storybook/addon-links';
-import { storiesOf } from '@storybook/react';
-
-// @ts-ignore
-import { Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
