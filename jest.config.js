770ae07e6964ca5addb3b977671ba81d75ea75c0
casserni <nicholascassera@gmail.com>
2019-03-28 10:29:57 -0500

    chore(snapshot): update snapshot

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupTestFrameworkScriptFile: './setupTests.ts',
   snapshotSerializers: ['enzyme-to-json/serializer'],
+  moduleNameMapper: {
+    '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
+  },
 };
