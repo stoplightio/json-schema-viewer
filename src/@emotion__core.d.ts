7df1ff2fa4f08fd926ee58e1179ef2b218fccae2
Jakub Rożek <jakub@stoplight.io>
2019-04-07 23:03:06 +0200

    feat: bump dependencies

--- src/@emotion__core.d.ts
+++
@@ -1,8 +0,0 @@
-// a dumb stub, workaround for storybook emotion problem
-// can be removed once Storybook v5 is released and we can have own @emotion/core installed
-declare module '@emotion/core' {
-  import { createElement } from 'react';
-
-  export const jsx: typeof createElement;
-  export const css: Function;
-}
