f6be200c39685bb84bff88c0947851b42e69d770
Jakub Rożek <jakub@stoplight.io>
2019-05-16 21:40:03 +0200

    chore: lodash imports

--- src/utils/isSchemaViewerEmpty.ts
+++ src/utils/isSchemaViewerEmpty.ts
@@ -1,5 +1,5 @@
 import { ISchema } from '@stoplight/types';
-import { get, isEmpty } from 'lodash';
+import { get as _get, isEmpty as _isEmpty } from 'lodash';
 
 const combinerTypes = ['allOf', 'oneOf', 'anyOf'];
 
@@ -7,7 +7,7 @@ export const isSchemaViewerEmpty = (schema: ISchema) => {
   const objectKeys = Object.keys(schema);
 
   if (objectKeys.length === 1 && combinerTypes.includes(objectKeys[0])) {
-    return isEmpty(get(schema, objectKeys[0], []));
+    return _isEmpty(_get(schema, objectKeys[0], []));
   }
 
   return false;
