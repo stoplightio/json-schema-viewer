177b5fe1de7d42965dd11ea0eb83447624bb912a
Chris <chrisnlott@gmail.com>
2019-05-20 17:28:01 -0500

    fix: negative margin-left instead of absolute left

--- src/components/SchemaRow.tsx
+++ src/components/SchemaRow.tsx
@@ -72,8 +72,8 @@ export const SchemaRow: React.FunctionComponent<ISchemaRow> = ({ node, treeStore
           )}
 
         {schemaNode.divider && (
-          <div className="flex items-center w-full absolute" style={{ top: -9, height: 1, left: -15 }}>
-            <div className="text-darken-7 dark:text-lighten-7 pr-2 uppercase text-xs">{schemaNode.divider}</div>
+          <div className="flex items-center w-full absolute" style={{ top: -9, height: 1 }}>
+            <div className="text-darken-7 dark:text-lighten-7 uppercase text-xs pr-2 -ml-4">{schemaNode.divider}</div>
             <div className="flex-1 bg-darken-5 dark:bg-lighten-5" style={{ height: 1 }} />
           </div>
         )}
