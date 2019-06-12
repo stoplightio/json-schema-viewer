ae9cb8c40ec14ba4b1da480c52fe2620671150ce
Jakub Rożek <P0lip@users.noreply.github.com>
2019-06-12 23:27:15 +0800

    feat: dark theme improvements (#26)

--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -65,17 +65,17 @@ Types.displayName = 'JsonSchemaViewer.Types';
  * HELPERS
  */
 export const PropertyTypeColors: Dictionary<string, IType['type']> = {
-  object: 'text-blue-6',
+  object: 'text-blue-6 dark:text-blue-4',
   any: 'text-blue-5',
-  array: 'text-green-6',
+  array: 'text-green-6 dark:text-green-4',
   allOf: 'text-orange-5',
-  oneOf: 'text-orange-5',
   anyOf: 'text-orange-5',
+  oneOf: 'text-orange-5',
   null: 'text-orange-5',
-  integer: 'text-red-7',
-  number: 'text-red-7',
+  integer: 'text-red-7 dark:text-red-6',
+  number: 'text-red-7 dark:text-red-6',
   boolean: 'text-red-4',
   binary: 'text-green-4',
-  string: 'text-green-7',
-  $ref: 'text-purple-6',
+  string: 'text-green-7 dark:text-green-5',
+  $ref: 'text-purple-6 dark:text-purple-4',
 };
