4bcd27c4f4a2a24e632cbf07d277d966e9270751
Jakub Jankowski <jakubj@stoplight.io>
2019-08-29 11:36:31 +0200

    fix: array subtype (#46)
    * fix: setting array subtype
    
    * test: add Prorerty tests
    
    * chore: update inferType

--- src/utils/inferType.ts
+++ src/utils/inferType.ts
@@ -1,7 +1,11 @@
 import { JSONSchema4, JSONSchema4TypeName } from 'json-schema';
 import { SchemaKind } from '../types';
 
-export function inferType(node: JSONSchema4): JSONSchema4TypeName | undefined {
+export function inferType(node: JSONSchema4): JSONSchema4TypeName | JSONSchema4TypeName[] | undefined {
+  if ('type' in node) {
+    return node.type;
+  }
+
   if ('properties' in node) {
     return SchemaKind.Object;
   }
