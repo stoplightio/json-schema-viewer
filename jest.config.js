6c36cace3c42d6ac69929e27e9dba74acdb75783
Jakub Rożek <jakub@stoplight.io>
2020-02-14 21:18:41 +0700

    revert: babel.config.js

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,10 @@ module.exports = {
     '\\.css$': '<rootDir>/__mocks__/styleMock.js'
   },
   testMatch: ['<rootDir>/src/**/__tests__/*.(ts|js)?(x)'],
+  transformIgnorePatterns: ['/node_modules/(?!lodash-es)/'],
   transform: {
-    '\\.tsx?$': 'ts-jest'
+    '\\.tsx?$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest'
   },
   coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/', '/__stories__/', '__mocks__/', 'types.ts'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
