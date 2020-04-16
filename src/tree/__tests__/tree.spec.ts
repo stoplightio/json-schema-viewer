dee59bc1e86ded26eb3819d9784ae5d8e89e8745
Jakub Rożek <jakub@stoplight.io>
2020-04-16 02:57:49 +0200

    fix: generate correct paths for $ref array items

--- src/tree/__tests__/tree.spec.ts
+++ src/tree/__tests__/tree.spec.ts
@@ -107,7 +107,7 @@ describe('SchemaTree', () => {
       });
     });
 
-    describe('array with refed items', () => {
+    describe('array with $reffed items', () => {
       let tree: SchemaTree;
       let schema: JSONSchema4;
 
@@ -161,6 +161,104 @@ describe('SchemaTree', () => {
           }),
         );
       });
+    });
+  });
+
+  describe('paths generation', () => {
+    let schema: JSONSchema4;
+    let tree: SchemaTree;
+
+    beforeEach(() => {
+      schema = {
+        type: 'object',
+        properties: {
+          user: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+              address: {
+                type: 'string',
+              },
+            },
+          },
+          permissions: {
+            type: 'array',
+            items: {
+              $ref: '#/properties/user',
+            },
+          },
+        },
+      };
+
+      tree = new SchemaTree(schema, new SchemaTreeState(), {
+        expandedDepth: Infinity,
+        mergeAllOf: false,
+        resolveRef: void 0,
+      });
+    });
+
+    test('for root', () => {
+      tree.populate();
+
+      expect(getNodeMetadata(tree.itemAt(0)!)).toHaveProperty('path', []);
+    });
+
+    test('for plain object member', () => {
+      tree.populate();
+
+      expect(getNodeMetadata(tree.itemAt(1)!)).toHaveProperty('path', [
+        'properties',
+        'user'
+      ]);
+    });
+
+    test('for deep object member', () => {
+      tree.populate();
+
+      expect(getNodeMetadata(tree.itemAt(2)!)).toHaveProperty('path', [
+        'properties',
+        'user',
+        'properties',
+        'name',
+      ]);
+    });
+
+    test('for array items', () => {
+      tree.populate();
+
+      expect(getNodeMetadata(tree.itemAt(tree.count - 1)!)).toHaveProperty('path', [
+        'properties',
+        'permissions',
+      ]);
+    });
+
+    test('for $reffed array items', () => {
+      tree.populate();
+      tree.unwrap(tree.itemAt(tree.count - 1) as TreeListParentNode);
+
+      expect(getNodeMetadata(tree.itemAt(tree.count - 3)!)).toHaveProperty('path', [
+        'properties',
+        'permissions',
+        'items',
+      ]);
+
+      expect(getNodeMetadata(tree.itemAt(tree.count - 2)!)).toHaveProperty('path', [
+        'properties',
+        'permissions',
+        'items',
+        'properties',
+        'name',
+      ]);
+
+      expect(getNodeMetadata(tree.itemAt(tree.count - 1)!)).toHaveProperty('path', [
+        'properties',
+        'permissions',
+        'items',
+        'properties',
+        'address',
+      ]);
     });
   });
 });
