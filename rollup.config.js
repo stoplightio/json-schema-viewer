e8da1ce27d258d3c5440dd98107ffad7607e0d09
Jakub Rożek <jakub@stoplight.io>
2020-05-13 14:59:38 +0200

    chore(rollup): bundle tree

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,42 @@
-import config from '@stoplight/scripts/rollup.config';
-import commonjs from 'rollup-plugin-commonjs';
+import * as path from 'path';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 
-config.plugins.push(commonjs());
+const BASE_PATH = process.cwd();
 
-export default config;
+const plugins = [
+  typescript({
+    tsconfig: path.resolve(BASE_PATH, 'tsconfig.build.json'),
+    include: ['src/**/*.{ts,tsx}'],
+  }),
+  terser(),
+  commonjs(),
+];
+
+export default [
+  {
+    input: path.resolve(BASE_PATH, 'src/index.ts'),
+    plugins,
+    output: [
+      {
+        file: path.resolve(BASE_PATH, 'dist/index.cjs.js'),
+        format: 'cjs',
+      },
+      {
+        file: path.resolve(BASE_PATH, 'dist/index.es.js'),
+        format: 'esm',
+      },
+    ],
+  },
+  {
+    input: path.resolve(BASE_PATH, 'src/tree/index.ts'),
+    plugins,
+    output: [
+      {
+        file: path.resolve(BASE_PATH, 'dist/tree/index.js'),
+        format: 'esm',
+      },
+    ],
+  },
+];
