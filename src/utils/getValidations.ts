1da1acfbf54b10c22eee60bc3f4c4f77bf13fad5
mmiask <mmiask92@gmail.com>
2020-11-04 18:06:52 +0100

    fix: rever change in getValidations

--- src/utils/getValidations.ts
+++ src/utils/getValidations.ts
@@ -4,7 +4,7 @@ import { flatMap as _flatMap, pick as _pick } from 'lodash';
 
 export const COMMON_VALIDATION_TYPES = [
   'enum', // https://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-5.5.1
-  'format',
+  'format', // https://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-7
   'default',
   'example',
   'nullable',
