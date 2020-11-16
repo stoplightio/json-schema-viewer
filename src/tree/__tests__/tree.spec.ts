d445a1f2fff7504975d60a7f2ee41f1ab897613e
Jakub Rożek <jakub@stoplight.io>
2020-11-16 14:23:08 +0100

    revert: tree tests should remain in repo until json-schema-tree is used

--- src/tree/__tests__/tree.spec.ts
+++ src/tree/__tests__/tree.spec.ts
@@ -1,6 +1,9 @@
 import { TreeListParentNode } from '@stoplight/tree-list';
+import * as fs from 'fs';
 import { JSONSchema4 } from 'json-schema';
+import * as path from 'path';
 import { ResolvingError } from '../../errors';
+import { ViewMode } from '../../types';
 import { getNodeMetadata } from '../metadata';
 import { SchemaTree, SchemaTreeState } from '../tree';
 import { printTree } from './utils/printTree';
@@ -657,7 +660,7 @@ describe('SchemaTree', () => {
       });
 
       test('given very complex model with circular references, should bail out and display unmerged allOf', () => {
-        const schema = require('./__fixtures__/complex-allOf-model.json');
+        const schema = require('../../__fixtures__/complex-allOf-model.json');
 
         const tree = new SchemaTree(schema, new SchemaTreeState(), {
           expandedDepth: Infinity,
@@ -1244,5 +1247,247 @@ describe('SchemaTree', () => {
       expect(onPopulate).nthCalledWith(1, tree, tree.root);
       expect(onPopulate).nthCalledWith(2, tree, tree.itemAt(0));
     });
+  });
+
+  describe('tree correctness', () => {
+    // you can put tests verifying whether we generate expected tree
+    test('given multiple object and string type, should process properties', () => {
+      const schema: JSONSchema4 = {
+        type: ['string', 'object'],
+        properties: {
+          ids: {
+            type: 'array',
+            items: {
+              type: 'integer',
+            },
+          },
+        },
+      };
+
+      const tree = new SchemaTree(schema, new SchemaTreeState(), {
+        expandedDepth: Infinity,
+        mergeAllOf: true,
+        resolveRef: void 0,
+        shouldResolveEagerly: true,
+        onPopulate: void 0,
+      });
+
+      tree.populate();
+      expect(printTree(tree)).toMatchInlineSnapshot(`
+        "└─ #
+           ├─ type
+           │  ├─ 0: string
+           │  └─ 1: object
+           └─ children
+              └─ 0
+                 └─ #/properties/ids
+                    ├─ type: array
+                    └─ subtype: integer
+        "
+      `);
+    });
+
+    test('given complex type that includes array and complex array subtype, should not ignore subtype', () => {
+      const schema: JSONSchema4 = {
+        type: 'object',
+        properties: {
+          items: {
+            type: ['null', 'array'],
+            items: {
+              type: ['string', 'number'],
+            },
+            description:
+              "This description can be long and should truncate once it reaches the end of the row. If it's not truncating then theres and issue that needs to be fixed. Help!",
+          },
+        },
+      };
+
+      const tree = new SchemaTree(schema, new SchemaTreeState(), {
+        expandedDepth: Infinity,
+        mergeAllOf: true,
+        resolveRef: void 0,
+        shouldResolveEagerly: true,
+        onPopulate: void 0,
+      });
+
+      tree.populate();
+      expect(printTree(tree)).toMatchInlineSnapshot(`
+        "└─ #
+           ├─ type: object
+           └─ children
+              └─ 0
+                 └─ #/properties/items
+                    ├─ type
+                    │  ├─ 0: null
+                    │  └─ 1: array
+                    └─ subtype
+                       ├─ 0: string
+                       └─ 1: number
+        "
+      `);
+    });
+
+    describe.each(['anyOf', 'oneOf'])('given %s combiner placed next to allOf', combiner => {
+      let schema: JSONSchema4;
+
+      beforeEach(() => {
+        schema = {
+          type: 'object',
+          title: 'Account',
+          allOf: [
+            {
+              type: 'object',
+              properties: {
+                type: {
+                  type: 'string',
+                  enum: ['admin', 'editor'],
+                },
+                enabled: {
+                  type: 'boolean',
+                  description: 'Is this account enabled',
+                },
+              },
+              required: ['type'],
+            },
+          ],
+          [combiner]: [
+            {
+              type: 'object',
+              title: 'Admin',
+              properties: {
+                root: {
+                  type: 'boolean',
+                },
+                group: {
+                  type: 'string',
+                },
+                expirationDate: {
+                  type: 'string',
+                },
+              },
+            },
+            {
+              type: 'object',
+              title: 'Editor',
+              properties: {
+                supervisor: {
+                  type: 'string',
+                },
+                key: {
+                  type: 'string',
+                },
+              },
+            },
+          ],
+        };
+      });
+
+      test('given allOf merging disabled, should preserve both combiners', () => {
+        const tree = new SchemaTree(schema, new SchemaTreeState(), {
+          expandedDepth: Infinity,
+          mergeAllOf: false,
+          resolveRef: void 0,
+          shouldResolveEagerly: true,
+          onPopulate: void 0,
+        });
+
+        tree.populate();
+        expect(printTree(tree)).toMatchSnapshot();
+      });
+
+      test('given allOf merging enabled, should merge contents of allOf combiners', () => {
+        const tree = new SchemaTree(schema, new SchemaTreeState(), {
+          expandedDepth: Infinity,
+          mergeAllOf: true,
+          resolveRef: void 0,
+          shouldResolveEagerly: true,
+          onPopulate: void 0,
+        });
+
+        tree.populate();
+        expect(printTree(tree)).toMatchSnapshot();
+      });
+    });
+
+    test.each(['standalone', 'read', 'write'])('given %s mode, should populate proper nodes', mode => {
+      const schema: JSONSchema4 = {
+        type: ['string', 'object'],
+        properties: {
+          id: {
+            type: 'string',
+            readOnly: true,
+          },
+          description: {
+            type: 'string',
+            writeOnly: true,
+          },
+        },
+      };
+
+      const tree = new SchemaTree(schema, new SchemaTreeState(), {
+        expandedDepth: Infinity,
+        mergeAllOf: true,
+        resolveRef: void 0,
+        shouldResolveEagerly: true,
+        onPopulate: void 0,
+        viewMode: mode as ViewMode,
+      });
+
+      tree.populate();
+      expect(tree.count).toEqual(mode === 'standalone' ? 3 : 2);
+    });
+
+    test.each(['array-of-allofs.json'])('should match %s', filename => {
+      const schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../__fixtures__', filename), 'utf8'));
+      const tree = new SchemaTree(schema, new SchemaTreeState(), {
+        expandedDepth: Infinity,
+        mergeAllOf: true,
+        resolveRef: void 0,
+        shouldResolveEagerly: true,
+        onPopulate: void 0,
+      });
+
+      tree.populate();
+      expect(printTree(tree)).toMatchSnapshot();
+    });
+  });
+
+  test('given visible $ref node, should try to inject the title immediately', () => {
+    const schema: JSONSchema4 = {
+      type: 'object',
+      properties: {
+        foo: {
+          $ref: '#/properties/user',
+        },
+        user: {
+          title: 'User',
+          type: 'object',
+          properties: {
+            name: {
+              type: 'string',
+            },
+          },
+        },
+      },
+    };
+
+    const tree = new SchemaTree(schema, new SchemaTreeState(), {
+      expandedDepth: 0,
+      mergeAllOf: false,
+      resolveRef: void 0,
+      shouldResolveEagerly: false,
+      onPopulate: void 0,
+    });
+
+    tree.populate();
+    // tree.unwrap(tree.itemAt(0) as TreeListParentNode);
+
+    expect(getNodeMetadata(tree.itemAt(1)!)).toHaveProperty(
+      'schemaNode',
+      expect.objectContaining({
+        $ref: '#/properties/user',
+        title: 'User',
+      }),
+    );
   });
 });
