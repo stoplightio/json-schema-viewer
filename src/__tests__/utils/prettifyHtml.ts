505ffbd13722bb4619b0d20fd78fd4600d9085d7
Marcell Toth <marcell@marcelltoth.net>
2021-03-09 18:27:18 +0100

    test: use textContent snapshots

--- src/__tests__/utils/prettifyHtml.ts
+++
@@ -1,9 +0,0 @@
-const prettier = require('prettier');
-
-export function prettifyHtml(html: string) {
-  return prettier.format(html, {
-    printWidth: 120,
-    parser: 'html',
-    htmlWhitespaceSensitivity: 'ignore',
-  });
-}
