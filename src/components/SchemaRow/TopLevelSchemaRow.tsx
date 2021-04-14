171dfda26e40544629cf6d3208539a6028f381b5
Marcell Toth <marcell@marcelltoth.net>
2021-04-14 12:30:36 +0200

    feat: special handling for top level arrays

--- src/components/SchemaRow/TopLevelSchemaRow.tsx
+++ src/components/SchemaRow/TopLevelSchemaRow.tsx
@@ -1,16 +1,30 @@
-import { isRegularNode } from '@stoplight/json-schema-tree';
+import { isRegularNode, RegularNode } from '@stoplight/json-schema-tree';
 import * as React from 'react';
 
+import { calculateChildrenToShow, isComplexArray } from '../../tree';
 import { ChildStack } from '../shared/ChildStack';
 import { SchemaRow, SchemaRowProps } from './SchemaRow';
 
 export const TopLevelSchemaRow: React.FC<SchemaRowProps> = ({ schemaNode, nestingLevel }) => {
+  const childNodes = React.useMemo(() => calculateChildrenToShow(schemaNode), [schemaNode]);
+
   // regular objects are flattened at the top level
-  if (isRegularNode(schemaNode) && schemaNode.primaryType === 'object' && schemaNode.types?.length === 1) {
+  if (isRegularNode(schemaNode) && isPureObjectNode(schemaNode)) {
+    return <ChildStack childNodes={childNodes} currentNestingLevel={nestingLevel} />;
+  }
+
+  if (isComplexArray(schemaNode) && isPureObjectNode(schemaNode.children[0])) {
     return (
-      <ChildStack childNodes={schemaNode.children ?? []} currentNestingLevel={nestingLevel} shouldIndent={false} />
+      <div className="sl-text-sm sl-relative">
+        <div className="sl-mr-2 sl-font-mono sl-font-bold">array of:</div>
+        {childNodes.length > 0 ? <ChildStack childNodes={childNodes} currentNestingLevel={nestingLevel} /> : null}
+      </div>
     );
   }
 
   return <SchemaRow schemaNode={schemaNode} nestingLevel={nestingLevel} />;
 };
+
+function isPureObjectNode(schemaNode: RegularNode) {
+  return schemaNode.primaryType === 'object' && schemaNode.types?.length === 1;
+}
