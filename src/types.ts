30b8a5a5435080c99d63fed356b716b64d952fb8
Jakub Rożek <jakub@stoplight.io>
2021-04-28 12:30:08 +0200

    feat: accept JSON Schema Draft 6 & Draft 7 (#139)
    * feat: accept JSON Schema Draft 6 & Draft 7
    
    * revert: reduce diff

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import type { ReferenceNode, SchemaNode } from '@stoplight/json-schema-tree';
+import { JSONSchema4, JSONSchema6, JSONSchema7 } from 'json-schema';
 import * as React from 'react';
 
 export type GoToRefHandler = (node: ReferenceNode) => void;
@@ -11,3 +12,5 @@ export interface SchemaRowProps {
 export type RowAddonRenderer = (props: SchemaRowProps) => React.ReactNode;
 
 export type ViewMode = 'read' | 'write' | 'standalone';
+
+export type JSONSchema = JSONSchema4 | JSONSchema6 | JSONSchema7;
