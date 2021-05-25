a70b855b621621d86134ff797472e971091ed4b6
mmiask <mmiask92@gmail.com>
2021-05-25 12:58:30 +0200

    feat: add markdown support in descriptions

--- src/components/shared/Description.tsx
+++ src/components/shared/Description.tsx
@@ -1,8 +1,8 @@
+import { MarkdownViewer } from '@stoplight/markdown-viewer';
 import * as React from 'react';
 
 export const Description: React.FunctionComponent<{ value: string }> = ({ value }) => (
-  // TODO (JJ): Add mosaic popover showing full description in MarkdownViewer
   <div className="sl-w-full sl-text-muted" title={value}>
-    {value}
+    <MarkdownViewer markdown={value} />
   </div>
 );
