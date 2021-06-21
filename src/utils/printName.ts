3ad5c8c7c4c7d417a893bec09099ded0c4189850
Marcell Toth <marcell@marcelltoth.net>
2021-06-21 13:45:35 +0200

    feat: better names for oneOf/anyOf selector

--- src/utils/printName.ts
+++ src/utils/printName.ts
@@ -1,23 +1,25 @@
+import { getLastPathSegment } from '@stoplight/json';
 import { isReferenceNode, isRegularNode, RegularNode, SchemaNodeKind } from '@stoplight/json-schema-tree';
+import capitalize from 'lodash/capitalize.js';
 
 import { isNonNullable } from '../guards/isNonNullable';
 import { isComplexArray, isPrimitiveArray } from '../tree';
 
-export function printName(schemaNode: RegularNode): string | null {
+export function printName(schemaNode: RegularNode): string | undefined {
   if (
     schemaNode.primaryType !== SchemaNodeKind.Array ||
     !isNonNullable(schemaNode.children) ||
     schemaNode.children.length === 0
   ) {
-    return schemaNode.title;
+    return schemaNode.title ?? getNodeNameFromOriginalRef(schemaNode);
   }
 
   return printArrayName(schemaNode);
 }
 
-function printArrayName(schemaNode: RegularNode): string | null {
+function printArrayName(schemaNode: RegularNode): string | undefined {
   if (!isNonNullable(schemaNode.children) || schemaNode.children.length === 0) {
-    return schemaNode.title;
+    return schemaNode.title ?? getNodeNameFromOriginalRef(schemaNode);
   }
 
   if (schemaNode.children.length === 1 && isReferenceNode(schemaNode.children[0])) {
@@ -52,9 +54,18 @@ function printArrayName(schemaNode: RegularNode): string | null {
       return `array of ${firstChild.primaryType}s`;
     } else if (firstChild.combiners?.length) {
       return `array of ${firstChild.combiners.join('/')}`;
+    } else if (getNodeNameFromOriginalRef(schemaNode)) {
+      return `array of ${getNodeNameFromOriginalRef(schemaNode)}-s`;
     }
     return 'array';
   }
 
-  return null;
+  return undefined;
+}
+
+function getNodeNameFromOriginalRef(node: RegularNode) {
+  if (typeof node.originalFragment.$ref === 'string') {
+    return capitalize(getLastPathSegment(node.originalFragment.$ref));
+  }
+  return undefined;
 }
