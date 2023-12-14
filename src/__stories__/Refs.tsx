271f6ddcb63ef36530144a450aed5d62dfb38318
Brenda Rearden <brendarearden@gmail.com>
2023-12-14 12:44:32 -0700

    fix(package.json): bump json-schema-tree version

--- src/__stories__/Refs.tsx
+++ src/__stories__/Refs.tsx
@@ -1,13 +1,16 @@
 import { Story } from '@storybook/react';
-import { JSONSchema4 } from 'json-schema';
+import { JSONSchema4, JSONSchema7 } from 'json-schema';
 import React from 'react';
 
 import { JsonSchemaProps, JsonSchemaViewer } from '../components/JsonSchemaViewer';
+import { defaultResolver } from '../utils/refResolver';
 
 const defaultSchema = require('../__fixtures__/default-schema.json');
 const refSchema = require('../__fixtures__/references/base.json');
 const nullRefSchema = require('../__fixtures__/references/nullish.json');
 const brokenRefArraySchema = require('../__fixtures__/arrays/of-refs.json');
+const allOfRefSchema = require('../__fixtures__/references/allOfReference.json');
+const fullAllOfRefDoc = require('../__fixtures__/references/fullAllOfReference.json');
 
 export default {
   component: JsonSchemaViewer,
@@ -26,3 +29,9 @@ Nullish.args = { schema: nullRefSchema as JSONSchema4 };
 
 export const Broken = Template.bind({});
 Broken.args = { schema: brokenRefArraySchema as JSONSchema4 };
+
+export const RefResolverAllOfRef = Template.bind({});
+RefResolverAllOfRef.args = {
+  schema: allOfRefSchema as JSONSchema7,
+  resolveRef: defaultResolver(fullAllOfRefDoc),
+};
