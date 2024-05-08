3a3a9286512db1b0978c453500231b272d4d9c50
SB-harshitajadhav <harshita.jadhav@smartbear.com>
2024-05-08 19:29:36 +0530

    fix: <binary> is not displaying in the docs

--- src/utils/getApplicableFormats.ts
+++ src/utils/getApplicableFormats.ts
@@ -3,6 +3,12 @@ import { RegularNode, SchemaNodeKind } from '@stoplight/json-schema-tree';
 import { COMMON_JSON_SCHEMA_AND_OAS_FORMATS } from '../consts';
 
 export function getApplicableFormats(schemaNode: RegularNode): [type: SchemaNodeKind, format: string] | null {
+  // Format is handled for contentMediaType type i.e. binary format
+  if (schemaNode.types !== null && schemaNode.fragment['contentMediaType'] === 'application/octet-stream') {
+    for (const type of schemaNode.types) {
+      return [type, 'binary'];
+    }
+  }
   if (schemaNode.format === null) {
     return null;
   }
